6d11dc846302bc2739d2841fad8ae4693042eee4
kevin <ngs816@gmail.com>
2018-04-14 15:04:55 +0800

    better

--- hello/views.py
+++ hello/views.py
@@ -6,12 +6,12 @@ import requests
 # Create your views here.
 def index(request):
     
-    query = 'http://www.patentsview.org/api/patents/query?q={"_and":[{"inventor_last_name":"Jobs"},{"assignee_lastknown_country":"US"}]}&f=["patent_number"]'
-    #query = 'https://api.mlab.com/api/1/databases/yjjuan01/collections/test?apiKey=ITT5lmoZVIkElhBjGP7IYrreM4Jv0OmI'
+    #query = 'http://www.patentsview.org/api/patents/query?q={"_and":[{"inventor_last_name":"Jobs"},{"assignee_lastknown_country":"US"}]}&f=["patent_number"]'
+    query = 'https://api.mlab.com/api/1/databases/yjjuan01/collections/test?apiKey=ITT5lmoZVIkElhBjGP7IYrreM4Jv0OmI'
     
-    #result = requests.get(query)
-    #response = json.loads(result.text)
-    return HttpResponse('hello')
+    result = requests.get(query)
+    response = json.loads(result.text)
+    return HttpResponse(response[-1])
     #return render(request, 'index.html')
     #return "hello"
 
