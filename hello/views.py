ff5b55cd112d344771e94184f5eb1a357ec0f7dd
kevin <ngs816@gmail.com>
2018-04-14 14:48:01 +0800

    better

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-import requests
-
+#
 
 # Create your views here.
 def index(request):
@@ -10,9 +9,9 @@ def index(request):
     query = 'http://www.patentsview.org/api/patents/query?q={"_and":[{"inventor_last_name":"Jobs"},{"assignee_lastknown_country":"US"}]}&f=["patent_number"]'
     #query = 'https://api.mlab.com/api/1/databases/yjjuan01/collections/test?apiKey=ITT5lmoZVIkElhBjGP7IYrreM4Jv0OmI'
     
-    result = requests.get(query)
+    #result = requests.get(query)
     #response = json.loads(result.text)
-    return HttpResponse(result)
+    return HttpResponse('hello')
     #return render(request, 'index.html')
     #return "hello"
 
