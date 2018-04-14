966ba6dfeea0fc3d996474cc43bd93102a5ec637
kevin <ngs816@gmail.com>
2018-04-14 14:18:32 +0800

    better

--- hello/views.py
+++ hello/views.py
@@ -8,11 +8,11 @@ import json
 def index(request):
     
     #query = 'http://www.patentsview.org/api/patents/query?q={"_and":[{"inventor_last_name":"Jobs"},{"assignee_lastknown_country":"US"}]}&f=["patent_number"]'
-    query = 'https://api.mlab.com/api/1/databases/yjjuan01/collections/test?apiKey=ITT5lmoZVIkElhBjGP7IYrreM4Jv0OmI'
+    #query = 'https://api.mlab.com/api/1/databases/yjjuan01/collections/test?apiKey=ITT5lmoZVIkElhBjGP7IYrreM4Jv0OmI'
     
     result = requests.get(query)
-    response = json.loads(result.text)
-    return HttpResponse(response[-1])
+    #response = json.loads(result.text)
+    return HttpResponse(result)
     #return render(request, 'index.html')
     #return "hello"
 
