833ebb54bcebd49109208a36ceb5968e39b81360
kev <ngs816@gmail.com>
2019-07-22 22:26:23 +0800

    'better'

--- hello/views.py
+++ hello/views.py
@@ -42,10 +42,11 @@ def result(request):
              'http://rss.sciencedirect.com/publication/science/01681699',
              'http://rss.sciencedirect.com/publication/science/03088146',
              'http://rss.sciencedirect.com/publication/science/09565663',
-             'http://rss.sciencedirect.com/publication/science/15662535',
-             'http://feeds.rsc.org/rss/ay',
-             'https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=11263&channel-name=International+Journal+of+Computer+Vision',
-             'https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=10462&channel-name=Artificial+Intelligence+Review']
+             #'http://rss.sciencedirect.com/publication/science/15662535',
+             #'http://feeds.rsc.org/rss/ay',
+             #'https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=11263&channel-name=International+Journal+of+Computer+Vision',
+             #'https://link.springer.com/search.rss?facet-content-type=Article&facet-journal-id=10462&channel-name=Artificial+Intelligence+Review'
+             ]
     
     for feed in feeds:
         response = requests.get(feed)
